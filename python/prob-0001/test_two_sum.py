86c3bc5d0345aa8f102055e06c656a8a014b097c
Maja Sieczko <majasieczko@Majas-MacBook-Air.local>
2024-01-06 13:38:35 +1100

    prob-0001 - Add initial test

--- python/prob-0001/test_two_sum.py
+++ python/prob-0001/test_two_sum.py
@@ -1,15 +1,24 @@
+'''
+    - assertEqual()
+    - assertTrue()
+    - assertFalse()
+    - assertRaises()
+'''
+
 import unittest
 from two_sum import TwoSum
 
 class TestTwoSum(unittest.TestCase):
     def testOne(self):
-        self.assertEqual(1, 1)
+        two_sum = TwoSum()
+        a = two_sum.solution_one([2,7,11,15], 9)
+        self.assertEqual(a, [0, 1])
 
     def testTwo(self):
         self.assertEqual(1, 1)
 
     def testThree(self):
-        self.assertEqual(1, 10)
+        self.assertEqual(1, 1)
 
 
 if __name__ == '__main__':
--- python/prob-0001/two_sum.py
+++ python/prob-0001/two_sum.py
@@ -1,7 +1,15 @@
 from typing import List
 
 class TwoSum:
-    def test(self, nums: List[int], target: int) -> List[int]:
-        return [0, 0]
+    def solution_one(self, nums: List[int], target: int) -> List[int]:
+        '''
+        Brute force
+        '''
+        
+        for a in nums:
+            b = target - a
+            if b in nums and (nums.index(a) != nums.index(b)):
+                return [nums.index(a), nums.index(b)]
 
+        return []
         
