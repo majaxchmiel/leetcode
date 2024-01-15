41e7530a1d1b0ad79fd4560e27e7a1d97ee0a42a
Maja Sieczko <majasieczko@Majas-MacBook-Air.local>
2024-01-15 22:25:46 +1100

    prob-0001 - Update tests

--- python/prob-0001/test_two_sum.py
+++ python/prob-0001/test_two_sum.py
@@ -9,16 +9,33 @@ import unittest
 from two_sum import TwoSum
 
 class TestTwoSum(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.two_sum = TwoSum()
+
     def testOne(self):
-        two_sum = TwoSum()
-        a = two_sum.solution_one([2,7,11,15], 9)
-        self.assertEqual(a, [0, 1])
+        nums = [2, 7, 11, 15]
+        target = 9
+
+        solution = self.two_sum.solution_1(nums, target)
+        expected = [0, 1]
+        self.assertEqual(solution, expected)
 
     def testTwo(self):
-        self.assertEqual(1, 1)
+        nums = [3, 2, 4]
+        target = 6
+
+        solution = self.two_sum.solution_1(nums, target)
+        expected = [1, 2]
+        self.assertEqual(solution, expected)
 
     def testThree(self):
-        self.assertEqual(1, 1)
+        nums = [3, 3]
+        target = 6
+
+        solution = self.two_sum.solution_1(nums, target)
+        expected = [0, 1]
+        self.assertEqual(solution, expected)
 
 
 if __name__ == '__main__':
--- python/prob-0001/two_sum.py
+++ python/prob-0001/two_sum.py
@@ -1,15 +1,32 @@
 from typing import List
 
 class TwoSum:
-    def solution_one(self, nums: List[int], target: int) -> List[int]:
+    def solution_0(self, nums: List[int], target: int) -> List[int]:
+        for i in nums:
+            x = target - i
+            if x in nums and (nums.index(i) != nums.index(x)):
+                return [nums.index(i), nums.index(x)]
+
+    def solution_1(self, nums: List[int], target: int) -> List[int]:
         '''
         Brute force
-        '''
-        
-        for a in nums:
-            b = target - a
-            if b in nums and (nums.index(a) != nums.index(b)):
-                return [nums.index(a), nums.index(b)]
 
-        return []
+        Loop through each element x and find if there is another value that
+        equals to target - x. 
+        '''
+    
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+                
+    
+
+        """
+        for i, num_one in enumerate(nums):
+            # num_one = nums[i]
+            for j, num_two in enumerate(nums[i+1:]):
+                if num_one + num_two == target:
+                    return [i, j]
+        """
         
